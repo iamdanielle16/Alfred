e7e99158dc13f1037fbce8ee1d985e0cc8f95663
unknown <dnmcloug@gmail.com>
2018-11-21 18:39:21 +0000

    Added Sql Class

---
+++ PowerShell Modules/SqlClass.ps1
@@ -0,0 +1,33 @@
+﻿  Class Sql{
+
+    [MySql.Data.MySqlClient.MySqlConnection]ConnectDB([String]$MySqlInfo){
+        try{
+            $db = New-Object MySql.Data.MySqlClient.MySqlConnection;
+            $db.ConnectionString = $mysqlInfo;
+            $db.Open();
+        return $db
+        }
+        catch{
+            return $Error
+        }
+    }
+    
+    [System.Data.Dataset]QueryDB([MySql.Data.MySqlClient.MySqlConnection]$db, [String]$Query){
+        try{
+            $command = New-Object MySql.Data.MySqlClient.MySqlCommand($query, $db)
+            $dataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($command)
+            $dataSet = New-Object System.Data.DataSet
+            $recordCount = $dataAdapter.Fill($dataSet, "data")
+            $dataSet.Tables["data"] | Format-Table
+            return $dataSet
+        }
+        catch{
+            return $Error
+        }
+    }
+
+    CloseDB([MySql.Data.MySqlClient.MySqlConnection]$db){
+        $db.Close()
+    }
+
+}
